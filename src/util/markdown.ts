84e890b8636b0c546436bc4715faa8b7c0604c6f
Sergey <exconf@pop-os.localdomain>
2023-03-23 19:45:16 +0300

    Added converters for italic text and multiline code

--- src/util/markdown.ts
+++ src/util/markdown.ts
@@ -1,20 +1,21 @@
-const test1 =
-  "# header 1\r\nnormal text\r\n## header 2\r\nnormal text\r\n### header 3\r\nnormal text\r\n#### header 4\r\nnormal text\r\n##### header 5\r\nnormal text\r\n###### header 6\r\n\r\nnormal text\r\n\r\n**bold text** normal text **bold text** some text before ** blabla\r\nnormal text\r\n\r\n[Link text](https://app.bugsnag.com/link-address)\r\n\r\nnormal text\r\n_italic text_\r\n\r\n> quote text\r\n\r\n`single line of code`\r\n\r\n```\r\nmultiline code\r\nmultiline code\r\nmultiline code\r\n```\r\n\r\nnormal text\r\n\r\n- list item 1\r\n- list item 2\r\n- list item 3\r\n- list item 4\r\n\r\n1. ordered list item 1\r\n2. ordered list item 2\r\n3. ordered list item 3"
-console.log(convertMarkdown(test1))
-
 export function convertMarkdown(content: string): string {
   const blocks = content.split("\r\n")
   const processed: string[] = []
   const convertors = [
+    convertItalicText,
     convertHeaders,
     convertBoldText,
     convertUnorderedLists,
     convertLinkedTexts,
+    convertMultilineCode,
   ]
 
-  for (let block of blocks) {
-    for (let convertor of convertors) {
-      block = convertor(block)
+  for (let i = 0; i < blocks.length; i++) {
+    let block = blocks[i]
+    const prev = blocks[i - 1] ?? ""
+    const next = blocks[i + 1] ?? ""
+    for (const convertor of convertors) {
+      block = convertor(block, prev, next)
     }
     processed.push(block)
   }
@@ -22,7 +23,7 @@ export function convertMarkdown(content: string): string {
   return processed.join("\r\n")
 }
 
-function convertHeaders(block: string): string {
+function convertHeaders(block: string, _: string, __: string): string {
   const regex = new RegExp(/^#{1,6}\s/)
   if (regex.test(block)) {
     block = block.replace(regex, "*")
@@ -31,7 +32,7 @@ function convertHeaders(block: string): string {
   return block
 }
 
-function convertBoldText(block: string): string {
+function convertBoldText(block: string, _: string, __: string): string {
   const regex = new RegExp(/\*\*/g)
   const count = block.match(regex)?.length ?? 0
 
@@ -47,15 +48,42 @@ function convertBoldText(block: string): string {
   return block
 }
 
-function convertUnorderedLists(block: string): string {
+function convertUnorderedLists(block: string, _: string, __: string): string {
   if (block.startsWith("- ")) {
     block = block.replace("-", "•")
   }
   return block
 }
 
-function convertLinkedTexts(block: string): string {
+function convertLinkedTexts(block: string, _: string, __: string): string {
   const regex = /\[(.*?)\]\((.*?)\)/g
   block = block.replace(regex, "<$2|$1>")
+  return block
+}
+
+function convertItalicText(block: string, _: string, __: string): string {
+  const regex = /(?<!\*)\*([^*].*?)\*/g
+  block = block.replace(regex, "_$1_")
+  return block
+}
+
+function convertMultilineCode(
+  block: string,
+  prev: string,
+  next: string
+): string {
+  const regex = /^\s{4}/
+  if (!regex.test(block)) return block
+
+  block = block.replace(regex, "")
+
+  if (!regex.test(prev)) {
+    block = "```" + block
+  }
+
+  if (!regex.test(next)) {
+    block += "```"
+  }
+
   return block
 }
